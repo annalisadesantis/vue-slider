21c119f25cd970d3b4d73e2c3ec37c82467fb766
Annalisa <annalisadesantis770@gmail.com>
2020-11-09 16:27:04 +0100

    aggiunta commenti

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var app = new Vue({
     el: "#root",
+    // Mi permette di far partire una funzione l caricamento della pagina
+    created() {
+        this.autoplay();
+    },
     data: {
         titolo: "Prenota la tua visita",
         paragrafo: "Scopri la più grande collezione di opere di Vincent van Gogh al Museo Van Gogh di Amsterdam, con capolavori come I mangiatori di patate, Mandorlo in fiore e Il seminatore.",
@@ -8,22 +12,32 @@ var app = new Vue({
     },
     methods: {
         prev_image() {
+            // Prendo l'indice dell'immagine e lo decremento di 1
             this.indice_immagine -= 1;
+            // Prima che il valore diventa -1 lo portiamo in avanti
             if(this.indice_immagine < 0) {
                 this.indice_immagine = this.immagini.length - 1;
             }
         },
         next_image() {
+            // Prendo l'indice dell'immagine e lo incremento di 1
             this.indice_immagine += 1;
+            // Prima che il valore diventa 4 lo portiamo indietro
             if(this.indice_immagine > this.immagini.length - 1) {
             this.indice_immagine = 0;
             }
         },
-        timing(){
-            this.indice_immagine += 1;
+        autoplay(){
+            // Creo una funzione autoplay la quale include un set Interval che richiama la funzione di next_image che già avevamo creato per le frecce
+            setInterval(() => {
+                this.next_image()
+            },5000);
         },
-        parte(){
-            setTimeout(timing, 1000);
+        // Creo una funzione con un parametro
+        pallini(i){
+            // L'indice dell'immagine deve essere uguale al parametro che gli ho passato 
+            this.indice_immagine = i;
+            console.log(i);
         }
     }
 });
